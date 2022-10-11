f1c845703d69cdd15dded48e0b6c878689353d34
olkovtar <79195343+olkovtar@users.noreply.github.com>
2022-10-11 17:00:26 +0300

    Update server.py
    Lab 1, Task 1, Part 2 ( Server )

--- Lab1/server.py
+++ Lab1/server.py
@@ -1,8 +1,9 @@
 import socket
 from datetime import datetime
+import time
 
-host = socket.gethostname()
-port = 5000
+host = 'localhost'
+port = 16789
 
 address = socket.socket()
 address.bind((host, port))
@@ -11,12 +12,16 @@ print('Starting the server at', datetime.now())
 
 address.listen(2)
 
-conn, addr = address.accept()  # accept new connection
+conn, addr = address.accept()
 
 print("Connection from: " + str(addr))
 
 data = conn.recv(1024).decode()
 
 print("From connected user: " + str(data) + "  received at", datetime.now())
+
+if data:
+    time.sleep(5)
+    conn.send(data.encode())
 
 conn.close()
