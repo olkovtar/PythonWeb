b05179b6b552fd3577bd840b541cc9f31868401f
olkovtar <79195343+olkovtar@users.noreply.github.com>
2022-10-18 17:17:09 +0300

    Delete Lab2 directory

--- Lab2/httpserver.py
+++
@@ -1,5 +0,0 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-server_address = ("", 8000)
-print('=== Start local webserver ===')
-httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-httpd.serve_forever()
