7acd2529c74b9cf3b9194874e6c41a7bb260b790
olkovtar <79195343+olkovtar@users.noreply.github.com>
2024-02-09 23:33:16 +0200

    Update config.py

--- Koval_project/config.py
+++ Koval_project/config.py
@@ -6,13 +6,13 @@ class Config:
     DEVELOPMENT = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'test_secret_key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    WTF_CSRF_ENABLED = False
+    WTF_CSRF_ENABLED = True
 
 
 class DevConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    WTF_CSRF_ENABLED = False
+    WTF_CSRF_ENABLED = True
     if os.getenv('DATABASE_URL'):
         SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
     else:
@@ -22,12 +22,12 @@ class DevConfig(Config):
 class TestConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    WTF_CSRF_ENABLED = False
+    WTF_CSRF_ENABLED = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test_site.db')
 
 
 class ProdConfig(Config):
-    WTF_CSRF_ENABLED = False
+    WTF_CSRF_ENABLED = True
     if os.getenv('DATABASE_URL'):
         SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
     else:
