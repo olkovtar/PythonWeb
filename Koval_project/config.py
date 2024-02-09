3d05c310d7bcc16adda83ed68c87bc03464911ae
olkovtar <79195343+olkovtar@users.noreply.github.com>
2024-02-09 23:25:43 +0200

    Update config.py

--- Koval_project/config.py
+++ Koval_project/config.py
@@ -1,10 +1,10 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+SECRET_KEY = '3704882b0f5a837d583f63511352ca96'
 class Config:
     DEVELOPMENT = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '3704882b0f5a837d583f63511352ca96'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
 
